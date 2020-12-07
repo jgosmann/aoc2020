18b642369da5321761779b942ffefe0ab8dea5df
Jan Gosmann <jan@hyper-world.de>
2020-12-07 20:00:15 +0100

    Add solution for day 7 part 1
    Maybe a little bit messier than usual, but I was a bit pressed for
    time today.

---
+++ day7.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+import re
+import sys
+from typing import Dict, Set
+
+
+@dataclass
+class BagNode:
+    color: str
+    may_be_contained_in: set
+
+    def __hash__(self):
+        return hash(self.color)
+
+
+def transitive_containment_hull(root: BagNode) -> Set[BagNode]:
+    return (
+        set(root.may_be_contained_in).union(
+        *(
+            transitive_containment_hull(node)
+            for node in root.may_be_contained_in
+        ))
+    )
+
+
+def get_node(name: str, graph: Dict[str, BagNode]) -> BagNode:
+    m = re.match(r"\s*(.*)\s+bags?\s*.?", name)
+    color = m.group(1)
+    if color not in graph_of_bags:
+        graph_of_bags[color] = BagNode(color=color, may_be_contained_in=set())
+    return graph_of_bags[color]
+
+
+graph_of_bags = {}
+
+for line in sys.stdin.readlines():
+    outer, inner_list = line.split("contain")
+
+    outer_node = get_node(outer, graph_of_bags)
+
+    for inner_with_count in inner_list.split(","):
+        m = re.match(r"\s*(\d+)\s+(.*)", inner_with_count)
+        if m:
+            count = int(m.group(1))
+            inner = m.group(2)
+            inner_node = get_node(inner, graph_of_bags)
+            inner_node.may_be_contained_in.add(outer_node)
+
+
+print(len(transitive_containment_hull(graph_of_bags["shiny gold"])))
