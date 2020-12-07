b42315b2b1b5c5b338ebe75a772f2b3e405cb04d
Jan Gosmann <jan@hyper-world.de>
2020-12-07 20:09:22 +0100

    Add solution for day 7 part 2

--- day7.py
+++ day7.py
@@ -2,12 +2,14 @@ from dataclasses import dataclass
 import re
 import sys
 from typing import Dict, Set
+from weakref import WeakSet
 
 
 @dataclass
 class BagNode:
     color: str
-    may_be_contained_in: set
+    may_be_contained_in: WeakSet  # Prevent cyclic strong references to allow GC by ref count
+    needs_to_contain: dict
 
     def __hash__(self):
         return hash(self.color)
@@ -23,11 +25,20 @@ def transitive_containment_hull(root: BagNode) -> Set[BagNode]:
     )
 
 
+def total_bags_inside(root: BagNode) -> int:
+    return sum(
+        count * (1 + total_bags_inside(node))
+        for node, count in root.needs_to_contain.items()
+    )
+
+
 def get_node(name: str, graph: Dict[str, BagNode]) -> BagNode:
     m = re.match(r"\s*(.*)\s+bags?\s*.?", name)
     color = m.group(1)
     if color not in graph_of_bags:
-        graph_of_bags[color] = BagNode(color=color, may_be_contained_in=set())
+        graph_of_bags[color] = BagNode(
+            color=color, may_be_contained_in=WeakSet(), needs_to_contain={}
+        )
     return graph_of_bags[color]
 
 
@@ -45,6 +56,9 @@ for line in sys.stdin.readlines():
             inner = m.group(2)
             inner_node = get_node(inner, graph_of_bags)
             inner_node.may_be_contained_in.add(outer_node)
+            outer_node.needs_to_contain[inner_node] = count
 
 
-print(len(transitive_containment_hull(graph_of_bags["shiny gold"])))
+root = graph_of_bags["shiny gold"]
+print(len(transitive_containment_hull(root)))
+print(total_bags_inside(root))
